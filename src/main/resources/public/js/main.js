26fa6e0c19864106ad9968bfb108caa0bf36e99e
kheifer <11204675+kheifer@users.noreply.github.com>
2017-08-31 06:53:57 -0700

    Update main.js

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,3 +1,25 @@
 $("#search-icon").click(function(){
  search-box.val('');
-                   });
+});
+
+var $r = $('input[type="range"]');
+var $ruler = $('<div class="rangeslider__ruler" />');
+
+// Initialize slider
+$r.rangeslider({
+  polyfill: false,
+  onInit: function() {
+    $ruler[0].innerHTML = getRulerRange(this.min, this.max, this.step);
+    this.$range.prepend($ruler);}
+});
+
+function getRulerRange(min, max, step) {
+  var range = '';
+  var i = 0;
+  
+  while (i <= max) {
+    range += i + ' ';
+    i = i + step;
+  }
+  return range;
+}
